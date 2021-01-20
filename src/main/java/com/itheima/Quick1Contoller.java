fa50415d48546494d06d5a8e9f5fee6ec943193a
zhangrr <350741524@qq.com>
2021-01-20 21:15:03 +0800

    SpringBoot_demo第一次提交

---
+++ src/main/java/com/itheima/Quick1Contoller.java
@@ -0,0 +1,21 @@
+package com.itheima;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Quick1Contoller {
+    @Value("${name}")
+    private String name;
+    @Value("${person.addr}")
+    private String addr;
+
+
+    @RequestMapping("/quick1")
+    @ResponseBody
+    public String quick1() {
+        return "name:"+name+"addr:"+addr;
+    }
+}
---
+++ src/main/java/com/itheima/Quick2Contoller.java
@@ -0,0 +1,37 @@
+package com.itheima;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ConfigurationProperties(prefix = "person")
+public class Quick2Contoller {
+
+    private String name;
+    private String addr;
+
+    @RequestMapping("/quick2")
+    @ResponseBody
+    public String quick1() {
+        return "name:"+name+"addr:"+addr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+}
