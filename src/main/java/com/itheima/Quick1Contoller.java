e8a83173b96d5aef3b60a71691961854a7b9bc4f
zhangrr <350741524@qq.com>
2021-01-20 21:49:39 +0800

    添加Quick3控制器

--- src/main/java/com/itheima/Quick1Contoller.java
+++ src/main/java/com/itheima/Quick1Contoller.java
@@ -7,15 +7,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Quick1Contoller {
+
     @Value("${name}")
     private String name;
     @Value("${person.addr}")
     private String addr;
-
-
     @RequestMapping("/quick1")
     @ResponseBody
     public String quick1() {
         return "name:"+name+"addr:"+addr;
     }
+
 }
---
+++ src/main/java/com/itheima/Quick3Controller.java
@@ -0,0 +1,45 @@
+package com.itheima;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author ：zhangrr
+ * @date ：Created in 2021/1/20 21:39
+ * @description：
+ * @modified By：
+ * @version:
+ */
+
+@Controller
+@ConfigurationProperties(prefix = "person")
+public class Quick3Controller {
+
+    private String name;
+    private String addr;
+
+    @RequestMapping("/quick3")
+    @ResponseBody
+    public String quick1() {
+        return "name:"+name+"addr:"+addr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+}
